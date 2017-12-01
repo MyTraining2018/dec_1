1af402b9ec618ecace5b938255fa211c47782c58
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2017-12-01 17:50:28 +0530

    Commit All

--- Day1-BankApp/src/main/java/org/cap/dto/Account.java
+++ Day1-BankApp/src/main/java/org/cap/dto/Account.java
@@ -48,22 +48,29 @@ public class Account {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj){
 			return true;
-		if (obj == null)
+		}
+		if (obj == null){
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()){
 			return false;
+		}
 		Account other = (Account) obj;
-		if (accountNo != other.accountNo)
+		if (accountNo != other.accountNo){
 			return false;
-		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
-			return false;
-		if (customer == null) {
-			if (other.customer != null)
+		}
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount)){
 				return false;
-		} else if (!customer.equals(other.customer))
+		}
+		if (customer == null) {
+			if (other.customer != null){
+				return false;
+			}
+		} else if (!customer.equals(other.customer)){
 			return false;
+		}
 		return true;
 	}
 
